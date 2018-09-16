0ea09fe02deaa6167356cec9372786ff2ce69eb0
Daniel <dan@dragomirweb.com>
2018-09-16 10:22:09 +0300

    add mock data

--- api/app.js
+++ api/app.js
@@ -15,15 +15,23 @@ app.get("/invoice", (req, res) => {
   res.send([
     {
       title: "factura-01",
+      client: "client 1",
       data: "18-08-2018"
     },
     {
       title: "factura-02",
+      client: "client 2",
       data: "19-08-2018"
     },
     {
       title: "factura-03",
+      client: "client 3",
       data: "20-08-2018"
+    },
+    {
+      title: "factura-04",
+      client: "client 4",
+      data: "22-08-2018"
     }
   ]);
 });
