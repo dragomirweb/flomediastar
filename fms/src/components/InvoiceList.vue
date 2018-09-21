c4f1c469302ac0092652e7a8307d36190da78105
Daniel <dan@dragomirweb.com>
2018-09-21 09:40:26 +0300

    add search and datepicker search

--- fms/src/components/InvoiceList.vue
+++ fms/src/components/InvoiceList.vue
@@ -2,16 +2,17 @@
 <div>
     <div class="w-100 mt-2">
         <div class="d-flex my-4">
-            <div class="d-flex align-items-end w-25 mr-2">
-                <b-form-input @input="searchInvoices" v-model="searchInvoice" type="text" placeholder="Cauta factura"></b-form-input>
+            <div class="d-flex align-items-end w-50 mr-2">
+                <b-form-input @click="clearSearch" v-model="searchInvoice" type="text" placeholder="Cauta factura"></b-form-input>
             </div>
-            <div class="w-25 d-flex">
-                    <datepicker class="mr-1" :bootstrap-styling="true" :value="searchInvoiceByDateStart"></datepicker>
-                    <datepicker  :bootstrap-styling="true" :value="searchInvoiceByDateEnd"></datepicker>
+            <div class="w-50 d-flex">
+                <datepicker class="mr-1" :bootstrap-styling="true" @closed="clearSearch" v-model="searchInvoiceByDateStart">
+                </datepicker>
+                <datepicker :bootstrap-styling="true" @closed="clearSearch" v-model="searchInvoiceByDateEnd"></datepicker>
             </div>
         </div>
         <div class="invoices">
-            <b-list-group v-for="invoice in visibleInvoices" :key="invoice.id" class="d-flex">
+            <b-list-group v-for="invoice in filteredInvoices" :key="invoice.id" class="d-flex">
                 <b-list-group-item class="mb-2">
                     <h3 class="text-monospace text-uppercase">{{ invoice.title }}</h3>
                     <h5 class="text-monospace text-uppercase">{{ invoice.client }}</h5>
@@ -31,15 +32,6 @@
                 </b-list-group-item>
             </b-list-group>
         </div>
-        <!-- <paginatron v-if="invoices.length>10" @change="updateItems" :items-per-page="5" :rotate="true" :items="invoices">
-            <div slot-scope="{ setPage, nextPage, prevPage, page, pages, hasNextPage, hasPrevPage, nextButtonEvents, prevButtonEvents, nextButtonAttrs, prevButtonAttrs }">
-                <div class="d-flex w-100 justify-content-center">
-                  <button class="btn btn-md btn-primary mr-2" v-on="prevButtonEvents" v-bind="prevButtonAttrs" >Inapoi</button>
-                
-                  <button class="btn btn-md btn-primary" v-on="nextButtonEvents" >Urmatoarele</button>
-                </div>
-            </div>
-        </paginatron> -->
     </div>
 </div>
 </template>
@@ -47,25 +39,23 @@
 <script>
 import jsPDF from 'jspdf'
 import InvoiceService from '@/services/InvoiceService'
-import Datepicker from 'vuejs-datepicker';
-import Paginatron from 'vue-paginatron'
+import Datepicker from 'vuejs-datepicker'
+import moment from 'moment'
+import _ from 'lodash'
 
 export default {
     name: 'InvoiceList',
     components: {
-        "Paginatron": Paginatron,
         "Datepicker": Datepicker
     },
     data() {
         return {
             invoices: [],
             indent: 10,
-            currentPage: 1,
-            perPage: 4,
-            visibleInvoices: [],
             searchInvoice: '',
             searchInvoiceByDateStart: new Date(),
             searchInvoiceByDateEnd: new Date(),
+            dateSelected: false,
         }
     },
     mounted() {
@@ -81,23 +71,38 @@ export default {
             doc.text("titlu", 10, this.indent);
             doc.save(pdfName + '-' + data + '.pdf');
         },
-        updateItems(activeItems) {
-            this.visibleInvoices = activeItems;
-        },
-        searchInvoices() {
-            let searchInvoice = this.searchInvoice;
-            this.visibleInvoices = this.invoices.filter(function(invoice){
-                if(invoice.title.includes(searchInvoice)){
-                    return invoice.title.includes(searchInvoice);
-                } else if(invoice.client.includes(searchInvoice)){
-                    return invoice.client.includes(searchInvoice);
-                }
-            });
-            console.log(this.visibleInvoices)
-            console.log(searchInvoice)
+        clearSearch() {
+            this.searchInvoice = '';
+            this.dateSelected = true;
         }
     },
     computed: {
+        filteredInvoices: function () {
+            var vm = this
+            var startDate = Date.parse(vm.searchInvoiceByDateStart);
+            var endDate = Date.parse(vm.searchInvoiceByDateEnd);
+
+            if (vm.searchInvoice != '') {
+                return vm.invoices.filter((invoice) => {
+                    return invoice.title.match(vm.searchInvoice) ||
+                        invoice.client.match(vm.searchInvoice);
+                });
+            } else if (startDate <= endDate && vm.searchInvoice == '' && vm.dateSelected == true) {
+                return _.filter(vm.invoices, (function (data) {
+                    if ((_.isNull(startDate) && _.isNull(endDate))) {
+                        return true
+                    } else {
+                        var date = Date.parse(data.data);
+                        return (date >= startDate && date <= endDate);
+                    }
+                }));
+            } else {
+                return _.filter(vm.invoices, (function (data) {
+                    return true;
+                }));
+            };
+        },
+
     }
 }
 </script>
