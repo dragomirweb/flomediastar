12fe5a779463fabb811075fef90ee27fde67252a
Dragomir Dan <dan@1stweb.dk>
2018-09-21 21:59:11 +0300

    add scroll bar styles

--- fms/src/App.vue
+++ fms/src/App.vue
@@ -1,39 +1,56 @@
 <template>
-  <div id="app">
+<div id="app">
     <Nav></Nav>
-    <router-view/>
-  </div>
+    <router-view />
+</div>
 </template>
 
 <script>
 import Nav from "@/components/Nav";
 
 export default {
-  name: 'App',
-  components: {
-    "Nav": Nav
-  }
+    name: 'App',
+    components: {
+        "Nav": Nav
+    }
 }
 </script>
 
-<style>
-  $box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2);
-  #app {
-    
-  }
+<style lang="scss">
+$box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2);
 
-  body {
+#app {}
+
+body {
     font-family: 'Roboto', Helvetica, Arial, sans-serif;
     font-size: 14px;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     height: 100vh;
     background-color: #eaeaea;
+    overflow-y: scroll;
+}
 
-  }
-
-  .bg-dark {
+.bg-dark {
     background: linear-gradient(to bottom, rgb(40, 50, 68) 0%, rgba(21, 26, 35, 0.95) 100%);
     box-shadow: $box-shadow;
-  }
+}
+
+body::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #F5F5F5;
+}
+
+body::-webkit-scrollbar {
+    width: 10px;
+    background-color: #F5F5F5;
+}
+
+body::-webkit-scrollbar-thumb {
+    background-color: rgb(40, 50, 68);
+
+    background-image: -webkit-gradient(linear, 0 0, 0 100%,
+        color-stop(.5, rgba(255, 255, 255, .2)),
+        color-stop(.5, transparent), to(transparent));
+}
 </style>
