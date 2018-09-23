695456fec7390631cdaf0801fbe2fe4b8c1d4380
Daniel <dan@dragomirweb.com>
2018-09-23 23:02:08 +0300

    update gen invoice component to use stores

--- fms/src/components/GenerateInvoice.vue
+++ fms/src/components/GenerateInvoice.vue
@@ -10,31 +10,31 @@
                 <b-list-group>
                     <b-list-group-item class="d-flex">
                         <b>Firma/Nume:</b>
-                        <span class="ml-auto">{{ form.firma }}</span>
+                        <span class="ml-auto">{{ factura.firma }}</span>
                     </b-list-group-item>
                     <b-list-group-item class="d-flex">
                         <b>Nr. Reg. Com:</b>
-                        <span class="ml-auto">{{ form.nrRegCom }}</span>
+                        <span class="ml-auto">{{ factura.nrRegCom }}</span>
                     </b-list-group-item>
                     <b-list-group-item class="d-flex">
                         <b>C.I.F:</b>
-                        <span class="ml-auto">{{ form.cif }}</span>
+                        <span class="ml-auto">{{ factura.cif }}</span>
                     </b-list-group-item>
                     <b-list-group-item class="d-flex">
                         <b>Sediul firmei:</b>
-                        <span class="ml-auto">{{ form.sediul }}</span>
+                        <span class="ml-auto">{{ factura.sediul }}</span>
                     </b-list-group-item>
                     <b-list-group-item class="d-flex">
                         <b>Cont bancar:</b>
-                        <span class="ml-auto">{{ form.contBancar }}</span>
+                        <span class="ml-auto">{{ factura.contBancar }}</span>
                     </b-list-group-item>
                     <b-list-group-item class="d-flex">
                         <b>Banca:</b>
-                        <span class="ml-auto">{{ form.banca }}</span>
+                        <span class="ml-auto">{{ factura.banca }}</span>
                     </b-list-group-item>
                     <b-list-group-item class="d-flex">
                         <b>Sucursala:</b>
-                        <span class="ml-auto">{{form.sucursala}}</span>
+                        <span class="ml-auto">{{factura.sucursala}}</span>
                     </b-list-group-item>
                 </b-list-group>
             </div>
@@ -49,38 +49,37 @@
         </b-modal>
         <b-form @submit="onSubmit" @reset="onReset" v-if="showClientForm">
             <b-form-group label="Firma/nume facturata:" label-for="clientFirma" class="mb-1">
-                <b-form-input id="clientFirma" type="text" v-model="form.firma" required placeholder="Firma/Nume">
+                <b-form-input id="clientFirma" type="text" v-model="factura.firma" required placeholder="Firma/Nume">
                 </b-form-input>
             </b-form-group>
             <b-form-group label="Nr. Reg. Com:" label-for="clientNrRegCom" class="mb-1">
-                <b-form-input id="clientNrRegCom" type="text" v-model="form.nrRegCom" required placeholder="Nr. Reg. Com">
+                <b-form-input id="clientNrRegCom" type="text" v-model="factura.nrRegCom" required placeholder="Nr. Reg. Com">
                 </b-form-input>
             </b-form-group>
             <b-form-group label="C.I.F:" label-for="clientCif" class="mb-1">
-                <b-form-input id="clientCif" type="text" v-model="form.cif" required placeholder="C.I.F">
+                <b-form-input id="clientCif" type="text" v-model="factura.cif" required placeholder="C.I.F">
                 </b-form-input>
             </b-form-group>
             <b-form-group label="Sediul firmei:" label-for="clientSediul" class="mb-1">
-                <b-form-input id="clientSediul" type="text" v-model="form.sediul" required placeholder="Sediul firmei">
+                <b-form-input id="clientSediul" type="text" v-model="factura.sediul" required placeholder="Sediul firmei">
                 </b-form-input>
             </b-form-group>
             <b-form-group label="Cont bancar:" label-for="clientContBancar" class="mb-1">
-                <b-form-input id="clientContBancar" type="text" v-model="form.contBancar" required placeholder="Cont bancar">
+                <b-form-input id="clientContBancar" type="text" v-model="factura.contBancar" required placeholder="Cont bancar">
                 </b-form-input>
             </b-form-group>
             <b-form-group label="Banca:" label-for="clientBanca" class="mb-1">
-                <b-form-input id="clientBanca" type="text" v-model="form.banca" required placeholder="Banca">
+                <b-form-input id="clientBanca" type="text" v-model="factura.banca" required placeholder="Banca">
                 </b-form-input>
             </b-form-group>
             <b-form-group label="Sucursala:" label-for="clientSucursala" class="mb-3">
-                <b-form-input id="clientSucursala" type="text" v-model="form.sucursala" required placeholder="Sucursala">
+                <b-form-input id="clientSucursala" type="text" v-model="factura.sucursala" required placeholder="Sucursala">
                 </b-form-input>
             </b-form-group>
             <b-button type="submit" variant="success">Salveaza</b-button>
             <b-button type="reset" variant="danger">Reseteaza formular</b-button>
         </b-form>
-        <div class="mt-4" v-if="!showClientForm" v-for="product in form.produse" :key="product.id">
-            <h1>{{product.id}}</h1>
+        <div class="mt-4" v-if="!showClientForm" v-for="product in factura.produse" :key="product.id">
             <b-form-group horizontal breakpoint="lg" label="" label-size="lg" label-class="font-weight-bold pt-0" class="mb-0 border-bottom border-secondary">
                 <b-form-group horizontal label="Produs sau serviciu:" label-class="text-sm-right" class="mb-0">
                     <b-form-input v-model="product.descriere" type="text"></b-form-input>
@@ -100,11 +99,11 @@
                 
             </b-form-group>
         </div>
-        <div class="d-flex flex-column align-items-end justify-content-end my-4 pr-2" v-if="!showClientForm && totalFacturaPlusTva != 0">
-            <div class="h5 mb-0"><span class="text-info h4">Total:</span> {{totalFactura.toFixed(2) | formatT}}</div>
-            <div class="h5 mb-0"><span class="text-info h4">TVA 19%:</span> {{totalFacturaTva.toFixed(2) | formatT}}</div>
+        <div class="d-flex flex-column align-items-end justify-content-end my-4 pr-2" v-if="!showClientForm && factura.totalFacturaPlusTva != 0">
+            <div class="h5 mb-0"><span class="text-info h4">Total:</span> {{factura.totalFactura.toFixed(2) | formatT}}</div>
+            <div class="h5 mb-0"><span class="text-info h4">TVA 19%:</span> {{factura.totalFacturaTva.toFixed(2) | formatT}}</div>
             <div class="h5 mb-0"><span class="text-info h4">Platit:</span> 0</div>
-            <div class="h5 "><span class="text-info h4">Total + TVA:</span> {{totalFacturaPlusTva.toFixed(2) | formatT}}</div>
+            <div class="h5 "><span class="text-info h4">Total + TVA:</span> {{factura.totalFacturaPlusTva.toFixed(2) | formatT}}</div>
         </div>
         <div class="d-flex" v-if="!showClientForm">
             <b-btn class="mt-3 w-auto mr-4" variant="outline-primary" block @click="addProduct">Adauga produs</b-btn>
@@ -116,8 +115,8 @@
 </template>
 
 <script>
-import InvoiceService from '@/services/InvoiceService'
 import InvoiceStatistics from '@/components/InvoiceStatistics'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
     name: 'GenerateInvoice',
@@ -126,7 +125,7 @@ export default {
     },
     data() {
         return {
-            form: {
+            factura: {
                 firma: '1',
                 nrRegCom: '1',
                 cif: '1',
@@ -140,19 +139,25 @@ export default {
                     pret: 0,
                     cantitatea: 0,
                     valoarea: 0
-                }]
+                }],
+                totalFactura: 0,
+                totalFacturaTva: 0,
+                totalFacturaPlusTva: 0
             },
-            showClientForm: true,
-            totalFactura: 0,
-            totalFacturaTva: 0,
-            totalFacturaPlusTva: 0
+            showClientForm: true
         }
+    },
+    created() {
+        this.$store.dispatch("storeInvoice")
     },
     updated () {
         this.test()
         this.getTotals()
     },
     methods: {
+        ...mapActions([
+            'storeInvoice'
+        ]),
         onSubmit(evt) {
             evt.preventDefault();
             // alert(JSON.stringify(this.form));
@@ -161,13 +166,13 @@ export default {
         onReset(evt) {
             evt.preventDefault();
             /* Reset our form values */
-            this.form.firma = '';
-            this.form.nrRegCom = '';
-            this.form.cif = '';
-            this.form.sediul = '';
-            this.form.contBancar = '';
-            this.form.banca = '';
-            this.form.sucursala = '';
+            this.factura.firma = '';
+            this.factura.nrRegCom = '';
+            this.factura.cif = '';
+            this.factura.sediul = '';
+            this.factura.contBancar = '';
+            this.factura.banca = '';
+            this.factura.sucursala = '';
             /* Trick to reset/clear native browser form validation state */
             this.showClientForm = false;
             this.$nextTick(() => {
@@ -185,19 +190,20 @@ export default {
             this.showClientForm = false;
         },
         addProduct() {
-            this.form.produse.push({
+            this.factura.produse.push({
                 descriere: 'Prestari servicii',
                 unitate: '',
                 pret: 0,
                 cantitatea: 0,
                 valoarea: 0
             });
+            console.log(this.factura)
         },
         removeLastProduct() {
-            this.form.produse.splice(-1, 1);
+            this.factura.produse.splice(-1, 1);
         },
         test() {
-            this.form.produse.forEach(el => {
+            this.factura.produse.forEach(el => {
                 el.valoarea = parseFloat(el.pret) * parseInt(el.cantitatea);
             });
         },
@@ -205,12 +211,12 @@ export default {
             var total = 0;
             var vm = this;
 
-            vm.form.produse.forEach(el => {
+            vm.factura.produse.forEach(el => {
                 total += el.valoarea;
             });
-            vm.totalFactura = parseFloat(total);
-            vm.totalFacturaTva = vm.totalFactura*0.19;
-            vm.totalFacturaPlusTva = vm.totalFactura + vm.totalFacturaTva;
+            vm.factura.totalFactura = parseFloat(total);
+            vm.factura.totalFacturaTva = vm.factura.totalFactura*0.19;
+            vm.factura.totalFacturaPlusTva = vm.factura.totalFactura + vm.factura.totalFacturaTva;
         }
     },
     computed: {
