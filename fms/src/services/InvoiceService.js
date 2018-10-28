70df098fff17efed3fe17f09bd0cc4454703d384
Daniel <dan@dragomirweb.com>
2018-10-28 22:38:16 +0200

    add api axios calls

--- fms/src/services/InvoiceService.js
+++ fms/src/services/InvoiceService.js
@@ -10,9 +10,20 @@ export default {
     saveInvoice(payload) {
       return Api().post('invoice', payload)
     },
+    updateInvoice(payload) {
+      return Api().put('invoice', payload)
+    },
     deleteInvoice(id) {
       return Api().delete('invoice', {
         params: { id: id }
         })
-    }
+    },
+    saveSituation(payload) {
+      return Api().post('situatie-lucrari', payload)
+    },
+    deleteSituation(id) {
+      return Api().delete('situatie-lucrari', {
+        params: { id: id }
+        })
+    },
   }
