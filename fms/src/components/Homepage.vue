ffcb8adb6579692e8bdf37ad31cd4b209782db7f
Daniel <dan@dragomirweb.com>
2018-10-29 22:22:46 +0200

    remove curs bnr

--- fms/src/components/Homepage.vue
+++ fms/src/components/Homepage.vue
@@ -6,23 +6,6 @@
         Data: {{ new Date() | moment("Do MMMM YYYY, h:mm:ss a") }}
       </div>
     </b-col>
-    <div class="col-12">
-      <h3>Curs BNR</h3>
-    </div>
-    <div class="col-6 d-flex">
-      <div class="mr-4">
-        <h6>EUR</h6>
-        <h6>USD</h6>
-        <h6>XAU</h6>
-        <h6>GBP</h6>
-      </div>
-      <div v-if="curs != []">
-        <h6  v-for="currency in curs" class="text-danger">{{ currency }}</h6>
-      </div>
-    </div>
-    <div class="col-6"> 
-
-    </div>
   </b-row>
 </b-container>
 </template>
@@ -30,40 +13,19 @@
 <script>
 import { mapActions, mapGetters } from 'vuex'
 import moment from 'moment'
-import axios from 'axios'
 
 export default {
   name: 'Homepage',
   data () {
     return {
-      valuta: {
-        eur: '',
-        usd: '',
-        xau: '',
-        gbp: ''
-      },
-      curs: []
     }
   },
   computed: {
   },
   mounted() {
-    this.getCurrency()
   },
   methods: {
-    getCurrency() {
-      const vm = this;
-      const url = 'http://www.infovalutar.ro/';
-      const date = moment(new Date()).format('YYYY/M/D');
-
-        for (let [key, value] of Object.entries(vm.valuta)) {
-          axios.get(
-            url + date + '/' + key + '.bnr'
-          ).then(
-            r => vm.curs.push(r.data)
-          )
-        }
-    }
+    
   }
 }
 </script>
